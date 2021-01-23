7af16d4f3b922b5c73c5960e1c0efce927b20369
ManicJamie <30422476+ManicJamie@users.noreply.github.com>
2021-01-23 15:30:13 +0000

    Add project files.

---
+++ ForContent/ForContent.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Modding;
+
+
+namespace ForContent
+{
+    /// <summary>
+    /// Disables isma's tear and lantern. Can be disabled from mod menu.
+    /// </summary>
+    public class ForContent : Mod, ITogglableMod
+    {
+
+        public override string GetVersion() => "1.0";
+
+        public override void Initialize()
+        {
+            ModHooks.Instance.GetPlayerBoolHook += PlayerBoolGet;
+        }
+
+        public void Unload()
+        {
+            ModHooks.Instance.GetPlayerBoolHook -= PlayerBoolGet;
+        }
+
+        // Overrides isma's and lantern bool gets.
+        public bool PlayerBoolGet(string target)
+        {
+            switch (target)
+            {
+                // Prevent game from reading isma's or lantern
+                case "hasAcidArmour":
+                case "hasLantern":
+                    return false;
+                // Allow other bools through
+                default:
+                    return PlayerData.instance.GetBoolInternal(target);
+            }
+            
+        }
+
+    }
+}
